9bcf0166770e5bd483878a029c85cdb70e6a6740
Ashritha145 <ashrithadontha1407@gmail.com>
2026-04-20 10:26:01 +0530

    updated the api key

--- app/src/main/java/com/example/newsnow/utlis/Constants.kt
+++ app/src/main/java/com/example/newsnow/utlis/Constants.kt
@@ -4,6 +4,6 @@ class Constants {
     companion object{
         val BASE_URL="https://newsapi.org/"
         val API_KEY="54d31e280f114a51bab91415ed674bed"
-        val GEMINI_API_KEY="AIzaSyDbGEEhuL6evcUkzgJTzHfZhJ6UJL22z50"
+        val GEMINI_API_KEY="AIzaSyB63VAQfXGJ4l0SnpqwTsXyz7xtZe7l9FA"
     }
 }
